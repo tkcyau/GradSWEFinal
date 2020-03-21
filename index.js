363c7281eefd8c83044d5b021e0b7a190f6b2774
Tim <tkcyau@gmail.com>
2020-03-21 17:01:17 -0400

    add contact form functuality

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/missionstatement", (req, res) => res.render("missionstatement"));
@@ -21,6 +26,40 @@ app.get("/social", (req, res) => res.render("social"));
 app.get("/outreach", (req, res) => res.render("outreach"));
 app.get("/contact", (req, res) => res.render("contact"));
 
+app.post("/send", (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>Message: ${req.body.message}</p>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "gradsweexec@gmail.com",
+      pass: "gradswe8"
+    }
+  });
+
+  const mailOptions = {
+    from: "gradsweexec@gmail.com", // sender address
+    to: "gradsweexec@gmail.com", // list of receivers
+    subject: "GradSWE Page Contact", // Subject line
+    html: output // plain text body
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.render("success");
+  });
+});
 // app.listen(process.env.port || port, () => {
 //   console.log("Server is running!");
 // });
